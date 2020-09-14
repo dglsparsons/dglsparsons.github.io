168545a00026a1cb4868e811dacdad03d41a759a
Douglas Parsons <dglsparsons@gmail.com>
2020-09-14 17:00:22 +0100

    Fix dem tests

--- test/test-homepage.js
+++ test/test-homepage.js
@@ -38,10 +38,10 @@ describe("check build output for homepage", () => {
       expect(navs.length).to.be.greaterThan(1);
     });
 
-    it("should have a list of posts", () => {
-      const posts = Array.from(doc.querySelectorAll("#posts ul li a"));
+    it("should have a list of articles", () => {
+      const articles = Array.from(doc.querySelectorAll("#articles ul li a"));
 
-      expect(posts.length).to.be.greaterThan(0);
+      expect(articles.length).to.be.greaterThan(0);
     });
   });
 });
